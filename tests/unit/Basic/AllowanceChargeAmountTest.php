209489508fdaf16c0791a9c2c6faffc1588f88f8
Valentin Leclere <valentin.leclere@tiimepdp.com>
2023-10-11 11:24:08 +0200

    fix basic tests

--- tests/unit/Basic/AllowanceChargeAmountTest.php
+++ tests/unit/Basic/AllowanceChargeAmountTest.php
@@ -3,10 +3,10 @@
 namespace Tiime\UniversalBusinessLanguage\Tests\unit\Basic;
 
 use Tiime\EN16931\DataType\CurrencyCode;
-use Tiime\UniversalBusinessLanguage\DataType\Basic\AllowanceAmount;
+use Tiime\UniversalBusinessLanguage\DataType\Basic\AllowanceChargeAmount;
 use Tiime\UniversalBusinessLanguage\Tests\helpers\BaseXMLNodeTestWithHelpers;
 
-class AllowanceAmountTest extends BaseXMLNodeTestWithHelpers
+class AllowanceChargeAmountTest extends BaseXMLNodeTestWithHelpers
 {
     protected const XML_VALID_CONTENT = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
@@ -36,8 +36,8 @@ XML;
     public function testCanBeCreatedFromFullContent(): void
     {
         $currentElement = $this->loadXMLDocument(self::XML_VALID_CONTENT);
-        $ublObject = AllowanceAmount::fromXML($this->xpath, $currentElement);
-        $this->assertInstanceOf(AllowanceAmount::class, $ublObject);
+        $ublObject = AllowanceChargeAmount::fromXML($this->xpath, $currentElement);
+        $this->assertInstanceOf(AllowanceChargeAmount::class, $ublObject);
         $this->assertEquals(36.01, $ublObject->getValue()->getFormattedValueRounded());
         $this->assertEquals(CurrencyCode::tryFrom("EUR"), $ublObject->getCurrencyCode());
     }
@@ -46,27 +46,27 @@ XML;
     {
         $this->expectException(\Exception::class);
         $currentElement = $this->loadXMLDocument(self::XML_INVALID_MISSING_CURRENCY);
-        AllowanceAmount::fromXML($this->xpath, $currentElement);
+        AllowanceChargeAmount::fromXML($this->xpath, $currentElement);
     }
 
     public function testCannotBeCreatedFromInvalid(): void
     {
         $this->expectException(\TypeError::class);
         $currentElement = $this->loadXMLDocument(self::XML_INVALID_AMOUNT);
-        AllowanceAmount::fromXML($this->xpath, $currentElement);
+        AllowanceChargeAmount::fromXML($this->xpath, $currentElement);
     }
 
     public function testCannotBeCreatedFromEmpty(): void
     {
         $this->expectException(\TypeError::class);
         $currentElement = $this->loadXMLDocument(self::XML_EMPTY_AMOUNT);
-        AllowanceAmount::fromXML($this->xpath, $currentElement);
+        AllowanceChargeAmount::fromXML($this->xpath, $currentElement);
     }
 
     public function testGenerateXml(): void
     {
         $currentElement = $this->loadXMLDocument(self::XML_VALID_CONTENT);
-        $ublObject = AllowanceAmount::fromXML($this->xpath, $currentElement);
+        $ublObject = AllowanceChargeAmount::fromXML($this->xpath, $currentElement);
         $rootDestination = $this->generateEmptyRootDocument();
         $rootDestination->appendChild($ublObject->toXML($this->document));
         $generatedOutput = $this->formatXMLOutput();
--- tests/unit/Basic/BaseQuantityTest.php
+++ tests/unit/Basic/BaseQuantityTest.php
@@ -9,53 +9,68 @@ use Tiime\UniversalBusinessLanguage\Tests\helpers\BaseXMLNodeTestWithHelpers;
 
 class BaseQuantityTest extends BaseXMLNodeTestWithHelpers
 {
-    protected const XML_VALID_CONTENT = <<<XML
+    protected const XML_VALID_FULL_CONTENT = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-  <cbc:InvoicedQuantity unitCode="C62">5.0000</cbc:InvoicedQuantity>
+  <cbc:BaseQuantity unitCode="C62">5.0000</cbc:BaseQuantity>
 </Invoice>
 XML;
 
-    protected const XML_INVALID_MISSING_UNIT_CODE = <<<XML
+    protected const XML_VALID_MINIMAL_CONTENT = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-  <cbc:InvoicedQuantity>5.0000</cbc:InvoicedQuantity>
+  <cbc:BaseQuantity>5.0000</cbc:BaseQuantity>
 </Invoice>
 XML;
 
-    protected const XML_INVALID_NO_LINE = <<<XML
+    protected const XML_VALID_NO_LINE = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-  </Invoice>
+</Invoice>
 XML;
 
-    public function testCanBeCreatedFromContent(): void
+    protected const XML_INVALID_WRONG_UNIT_CODE = <<<XML
+<Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
+  <cbc:BaseQuantity unitCode="WTF42">5.0000</cbc:BaseQuantity>
+</Invoice>
+XML;
+
+    public function testCanBeCreatedFromFullContent(): void
     {
-        $currentElement = $this->loadXMLDocument(self::XML_VALID_CONTENT);
+        $currentElement = $this->loadXMLDocument(self::XML_VALID_FULL_CONTENT);
         $ublObject = BaseQuantity::fromXML($this->xpath, $currentElement);
         $this->assertInstanceOf(BaseQuantity::class, $ublObject);
         $this->assertEquals(5, $ublObject->getQuantity()->getFormattedValueRounded());
         $this->assertEquals(UnitOfMeasurement::tryFrom("C62"), $ublObject->getUnitCode());
     }
 
-    public function testCannotBeCreatedFromMissingUnitCode(): void
+    public function testCanBeCreatedFromMinimalContent(): void
     {
-        $this->expectException(\Exception::class);
-        $currentElement = $this->loadXMLDocument(self::XML_INVALID_MISSING_UNIT_CODE);
-        BaseQuantity::fromXML($this->xpath, $currentElement);
+        $currentElement = $this->loadXMLDocument(self::XML_VALID_MINIMAL_CONTENT);
+        $ublObject = BaseQuantity::fromXML($this->xpath, $currentElement);
+        $this->assertInstanceOf(BaseQuantity::class, $ublObject);
+        $this->assertEquals(5, $ublObject->getQuantity()->getFormattedValueRounded());
+        $this->assertNull($ublObject->getUnitCode());
     }
 
-    public function testCannotBeCreatedFromEmpty(): void
+    public function testCanBeCreatedFromEmpty(): void
     {
-        $this->expectException(\TypeError::class);
-        $currentElement = $this->loadXMLDocument(self::XML_INVALID_NO_LINE);
+        $currentElement = $this->loadXMLDocument(self::XML_VALID_NO_LINE);
+        $ublObject = BaseQuantity::fromXML($this->xpath, $currentElement);
+        $this->assertNull($ublObject);
+    }
+
+    public function testCannotBeCreatedFromWrongUnitCode(): void
+    {
+        $this->expectException(\Exception::class);
+        $currentElement = $this->loadXMLDocument(self::XML_INVALID_WRONG_UNIT_CODE);
         BaseQuantity::fromXML($this->xpath, $currentElement);
     }
 
     public function testGenerateXml(): void
     {
-        $currentElement = $this->loadXMLDocument(self::XML_VALID_CONTENT);
+        $currentElement = $this->loadXMLDocument(self::XML_VALID_FULL_CONTENT);
         $ublObject = BaseQuantity::fromXML($this->xpath, $currentElement);
         $rootDestination = $this->generateEmptyRootDocument();
         $rootDestination->appendChild($ublObject->toXML($this->document));
         $generatedOutput = $this->formatXMLOutput();
-        $this->assertEquals(self::XML_VALID_CONTENT, $generatedOutput);
+        $this->assertEquals(self::XML_VALID_FULL_CONTENT, $generatedOutput);
     }
 }
