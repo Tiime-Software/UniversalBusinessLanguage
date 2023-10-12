19dc91c325822dec0ed9590d3642d4df46f19858
Valentin Leclere <valentin.leclere@tiimepdp.com>
2023-10-12 12:13:37 +0200

    add delivery nodes tests

--- tests/unit/Aggregate/DeliveryAddressTest.php
+++ tests/unit/Aggregate/DeliveryAddressTest.php
@@ -1,72 +1,68 @@
 <?php
 
-namespace Tiime\UniversalBusinessLanguage\Tests\unit\Aggregate;
 
+use Tiime\EN16931\DataType\Identifier\LocationIdentifier;
+use Tiime\EN16931\DataType\InternationalCodeDesignator;
 use Tiime\UniversalBusinessLanguage\DataType\Aggregate\AddressLine;
 use Tiime\UniversalBusinessLanguage\DataType\Aggregate\Country;
-use Tiime\UniversalBusinessLanguage\DataType\Aggregate\PostalAddress;
+use Tiime\UniversalBusinessLanguage\DataType\Aggregate\DeliveryAddress;
+use Tiime\UniversalBusinessLanguage\DataType\Aggregate\DeliveryLocation;
 use Tiime\UniversalBusinessLanguage\Tests\helpers\BaseXMLNodeTestWithHelpers;
 
-class PostalAddressTest extends BaseXMLNodeTestWithHelpers
+class DeliveryAddressTest extends BaseXMLNodeTestWithHelpers
 {
     protected const XML_VALID_FULL_CONTENT = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-  <cac:PostalAddress>
+  <cac:Address>
     <cac:Country>
-      <cbc:IdentificationCode>FR</cbc:IdentificationCode>
+      <cbc:IdentificationCode>SE</cbc:IdentificationCode>
     </cac:Country>
-    <cbc:StreetName>1, rue du fournisseur</cbc:StreetName>
-    <cbc:AdditionalStreetName>Cour du fournisseur</cbc:AdditionalStreetName>
-    <cbc:CityName>Quimper</cbc:CityName>
-    <cbc:PostalZone>29000</cbc:PostalZone>
-    <cbc:CountrySubentity>Bretagne</cbc:CountrySubentity>
+    <cbc:StreetName>Delivery Street 1</cbc:StreetName>
+    <cbc:AdditionalStreetName>Delivery Street 2</cbc:AdditionalStreetName>
+    <cbc:CityName>Malmö</cbc:CityName>
+    <cbc:PostalZone>86756</cbc:PostalZone>
+    <cbc:CountrySubentity>South Region</cbc:CountrySubentity>
     <cac:AddressLine>
-      <cbc:Line>BATIMENT DU FOURNISSEUR</cbc:Line>
+      <cbc:Line>C54</cbc:Line>
     </cac:AddressLine>
-  </cac:PostalAddress>
+  </cac:Address>
 </Invoice>
 XML;
 
     protected const XML_VALID_MINIMAL_CONTENT = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-  <cac:PostalAddress>
+  <cac:Address>
     <cac:Country>
-      <cbc:IdentificationCode>FR</cbc:IdentificationCode>
+      <cbc:IdentificationCode>SE</cbc:IdentificationCode>
     </cac:Country>
-  </cac:PostalAddress>
+  </cac:Address>
 </Invoice>
 XML;
 
-    protected const XML_INVALID_NO_COUNTRY = <<<XML
+    protected const XML_VALID_NO_LINE = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-  <cac:PostalAddress>
-  </cac:PostalAddress>
 </Invoice>
 XML;
 
-    protected const XML_INVALID_MULTIPLE_STREETNAME = <<<XML
+    protected const XML_INVALID_TOO_MANY_LINES = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-  <cac:PostalAddress>
-    <cbc:StreetName>1, rue du fournisseur</cbc:StreetName>
-    <cbc:StreetName>Cour du fournisseur</cbc:StreetName>
-    <cac:Country>
-      <cbc:IdentificationCode>FR</cbc:IdentificationCode>
-    </cac:Country>
-  </cac:PostalAddress>
+  <cac:DeliveryLocation>
+  </cac:DeliveryLocation>
+  <cac:DeliveryLocation>
+  </cac:DeliveryLocation>
 </Invoice>
 XML;
-
 
     public function testCanBeCreatedFromFullContent(): void
     {
         $currentElement = $this->loadXMLDocument(self::XML_VALID_FULL_CONTENT);
-        $ublObject = PostalAddress::fromXML($this->xpath, $currentElement);
-        $this->assertInstanceOf(PostalAddress::class, $ublObject);
-        $this->assertEquals("1, rue du fournisseur", $ublObject->getStreetName());
-        $this->assertEquals("Cour du fournisseur", $ublObject->getAdditionalStreetName());
-        $this->assertEquals("Quimper", $ublObject->getCityName());
-        $this->assertEquals("29000", $ublObject->getPostalZone());
-        $this->assertEquals("Bretagne", $ublObject->getCountrySubentity());
+        $ublObject = DeliveryAddress::fromXML($this->xpath, $currentElement);
+        $this->assertInstanceOf(DeliveryAddress::class, $ublObject);
+        $this->assertEquals("Delivery Street 1", $ublObject->getStreetName());
+        $this->assertEquals("Delivery Street 2", $ublObject->getAdditionalStreetName());
+        $this->assertEquals("Malmö", $ublObject->getCityName());
+        $this->assertEquals("86756", $ublObject->getPostalZone());
+        $this->assertEquals("South Region", $ublObject->getCountrySubentity());
         $this->assertInstanceOf(AddressLine::class, $ublObject->getAddressLine());
         $this->assertInstanceOf(Country::class, $ublObject->getCountry());
     }
@@ -74,8 +70,8 @@ XML;
     public function testCanBeCreatedFromMinimalContent(): void
     {
         $currentElement = $this->loadXMLDocument(self::XML_VALID_MINIMAL_CONTENT);
-        $ublObject = PostalAddress::fromXML($this->xpath, $currentElement);
-        $this->assertInstanceOf(Country::class, $ublObject->getCountry());
+        $ublObject = DeliveryAddress::fromXML($this->xpath, $currentElement);
+        $this->assertInstanceOf(DeliveryAddress::class, $ublObject);
         $this->assertNull($ublObject->getStreetName());
         $this->assertNull($ublObject->getAdditionalStreetName());
         $this->assertNull($ublObject->getCityName());
@@ -83,27 +79,26 @@ XML;
         $this->assertNull($ublObject->getCountrySubentity());
         $this->assertNull($ublObject->getAddressLine());
         $this->assertInstanceOf(Country::class, $ublObject->getCountry());
-
     }
 
-    public function testCannotBeCreatedFromNoCountry(): void
+    public function testCanBeCreatedFromNoLine(): void
     {
-        $this->expectException(\Exception::class);
-        $currentElement = $this->loadXMLDocument(self::XML_INVALID_NO_COUNTRY);
-        PostalAddress::fromXML($this->xpath, $currentElement);
+        $currentElement = $this->loadXMLDocument(self::XML_VALID_NO_LINE);
+        $ublObject = DeliveryAddress::fromXML($this->xpath, $currentElement);
+        $this->assertNull($ublObject);
     }
 
-    public function testCannotBeCreatedFromMultipleStreetName(): void
+    public function testCannotBeCreatedFromTooManyLines(): void
     {
         $this->expectException(\Exception::class);
-        $currentElement = $this->loadXMLDocument(self::XML_INVALID_MULTIPLE_STREETNAME);
-        PostalAddress::fromXML($this->xpath, $currentElement);
+        $currentElement = $this->loadXMLDocument(self::XML_INVALID_TOO_MANY_LINES);
+        DeliveryAddress::fromXML($this->xpath, $currentElement);
     }
 
     public function testGenerateXml(): void
     {
         $currentElement = $this->loadXMLDocument(self::XML_VALID_FULL_CONTENT);
-        $ublObject = PostalAddress::fromXML($this->xpath, $currentElement);
+        $ublObject = DeliveryAddress::fromXML($this->xpath, $currentElement);
         $rootDestination = $this->generateEmptyRootDocument();
         $rootDestination->appendChild($ublObject->toXML($this->document));
         $generatedOutput = $this->formatXMLOutput();
