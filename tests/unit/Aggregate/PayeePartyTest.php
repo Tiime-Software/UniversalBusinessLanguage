38fd02e4ba2f5666b3afb7a0a6486becaeaa387f
Valentin Leclere <valentin.leclere@tiimepdp.com>
2023-10-13 16:19:59 +0200

    add taxCategory test and fix payee party

--- tests/unit/Aggregate/PayeePartyTest.php
+++ tests/unit/Aggregate/PayeePartyTest.php
@@ -2,10 +2,6 @@
 
 namespace Tiime\UniversalBusinessLanguage\Tests\unit\Aggregate;
 
-use Tiime\UniversalBusinessLanguage\DataType\Aggregate\PartyName;
-use Tiime\UniversalBusinessLanguage\DataType\Aggregate\PayeeParty;
-use Tiime\UniversalBusinessLanguage\DataType\Aggregate\PayeePartyIdentification;
-use Tiime\UniversalBusinessLanguage\DataType\Aggregate\PayeePartyLegalEntity;
 use Tiime\UniversalBusinessLanguage\DataType\Aggregate\PayeePartyName;
 use Tiime\UniversalBusinessLanguage\Tests\helpers\BaseXMLNodeTestWithHelpers;
 
@@ -13,30 +9,17 @@ class PayeePartyTest extends BaseXMLNodeTestWithHelpers
 {
     protected const XML_VALID_FULL_CONTENT = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-  <cac:PayeeParty>
-    <cac:PartyIdentification>
-      <cbc:ID schemeID="SEPA">FR932874294</cbc:ID>
-    </cac:PartyIdentification>
-    <cac:PartyName>
-      <cbc:Name>Payee Name Ltd</cbc:Name>
-    </cac:PartyName>
-    <cac:PartyLegalEntity>
-      <cbc:CompanyID schemeID="0002">FR932874294</cbc:CompanyID>
-    </cac:PartyLegalEntity>
-  </cac:PayeeParty>
+  <cac:PartyName>
+    <cbc:Name>Payee Name Ltd</cbc:Name>
+  </cac:PartyName>
 </Invoice>
 XML;
 
     protected const XML_VALID_MINIMAL_CONTENT = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-  <cac:PayeeParty>
-    <cac:PartyIdentification>
-      <cbc:ID>FR932874294</cbc:ID>
-    </cac:PartyIdentification>
-    <cac:PartyName>
-      <cbc:Name>Payee Name Ltd</cbc:Name>
-    </cac:PartyName>
-  </cac:PayeeParty>
+  <cac:PartyName>
+    <cbc:Name>Payee Name Ltd</cbc:Name>
+  </cac:PartyName>
 </Invoice>
 XML;
 
@@ -47,63 +30,49 @@ XML;
 
     protected const XML_INVALID_MANY_LINES = <<<XML
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-  <cac:PayeeParty>
-    <cac:PartyIdentification>
-      <cbc:ID>FR932874294</cbc:ID>
-    </cac:PartyIdentification>
-    <cac:PartyName>
-      <cbc:Name>Payee Name Ltd</cbc:Name>
-    </cac:PartyName>
-  </cac:PayeeParty>
-  <cac:PayeeParty>
-    <cac:PartyIdentification>
-      <cbc:ID>FR932874294</cbc:ID>
-    </cac:PartyIdentification>
-    <cac:PartyName>
-      <cbc:Name>Payee Name Ltd</cbc:Name>
-    </cac:PartyName>
-  </cac:PayeeParty>
+  <cac:PartyName>
+    <cbc:Name>Payee Name Ltd</cbc:Name>
+  </cac:PartyName>
+  <cac:PartyName>
+    <cbc:Name>Payee Name Ltd</cbc:Name>
+  </cac:PartyName>
 </Invoice>
 XML;
 
     public function testCanBeCreatedFromFullContent(): void
     {
         $currentElement = $this->loadXMLDocument(self::XML_VALID_FULL_CONTENT);
-        $ublObject      = PayeeParty::fromXML($this->xpath, $currentElement);
-        $this->assertInstanceOf(PayeeParty::class, $ublObject);
-        $this->assertInstanceOf(PayeePartyIdentification::class, $ublObject->getPartyIdentification());
-        $this->assertInstanceOf(PartyName::class, $ublObject->getPartyName());
-        $this->assertInstanceOf(PayeePartyLegalEntity::class, $ublObject->getPartyLegalEntity());
+        $ublObject      = PayeePartyName::fromXML($this->xpath, $currentElement);
+        $this->assertInstanceOf(PayeePartyName::class, $ublObject);
+        $this->assertEquals('Payee Name Ltd', $ublObject->getName());
     }
 
     public function testCanBeCreatedFromMinimalContent(): void
     {
         $currentElement = $this->loadXMLDocument(self::XML_VALID_MINIMAL_CONTENT);
-        $ublObject      = PayeeParty::fromXML($this->xpath, $currentElement);
-        $this->assertInstanceOf(PayeeParty::class, $ublObject);
-        $this->assertInstanceOf(PayeePartyIdentification::class, $ublObject->getPartyIdentification());
-        $this->assertInstanceOf(PayeePartyName::class, $ublObject->getPartyName());
-        $this->assertNull($ublObject->getPartyLegalEntity());
+        $ublObject      = PayeePartyName::fromXML($this->xpath, $currentElement);
+        $this->assertInstanceOf(PayeePartyName::class, $ublObject);
+        $this->assertEquals('Payee Name Ltd', $ublObject->getName());
     }
 
-    public function testCanBeCreatedFromNoLine(): void
+    public function testCannotBeCreatedFromNoLine(): void
     {
+        $this->expectException(\Exception::class);
         $currentElement = $this->loadXMLDocument(self::XML_VALID_NO_LINE);
-        $ublObject      = PayeeParty::fromXML($this->xpath, $currentElement);
-        $this->assertNull($ublObject);
+        PayeePartyName::fromXML($this->xpath, $currentElement);
     }
 
     public function testCannotBeCreatedFromManyLines(): void
     {
         $this->expectException(\Exception::class);
         $currentElement = $this->loadXMLDocument(self::XML_INVALID_MANY_LINES);
-        PayeeParty::fromXML($this->xpath, $currentElement);
+        PayeePartyName::fromXML($this->xpath, $currentElement);
     }
 
     public function testGenerateXml(): void
     {
         $currentElement  = $this->loadXMLDocument(self::XML_VALID_FULL_CONTENT);
-        $ublObject       = PayeeParty::fromXML($this->xpath, $currentElement);
+        $ublObject       = PayeePartyName::fromXML($this->xpath, $currentElement);
         $rootDestination = $this->generateEmptyRootDocument();
         $rootDestination->appendChild($ublObject->toXML($this->document));
         $generatedOutput = $this->formatXMLOutput();
