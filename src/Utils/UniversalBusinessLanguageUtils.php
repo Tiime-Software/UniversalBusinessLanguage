2a90e80d1818d6a4344a5a580f9b4bacb1a1a48e
Aurélien Pillevesse <aurelien.pillevesse@tiimepdp.com>
2023-10-26 18:00:51 +0200

    add validateXSD function

--- src/Utils/UniversalBusinessLanguageUtils.php
+++ src/Utils/UniversalBusinessLanguageUtils.php
@@ -2,9 +2,25 @@
 
 declare(strict_types=1);
 
-namespace Tiime\UniversalBusinessLanguage\DataType\Utils;
+namespace Tiime\UniversalBusinessLanguage\Utils;
 
 class UniversalBusinessLanguageUtils
 {
     public const UBL_DATE_FORMAT = 'Y-m-d';
+
+    public const XSD_PATH = __DIR__ . \DIRECTORY_SEPARATOR . '..' . \DIRECTORY_SEPARATOR . '..' . \DIRECTORY_SEPARATOR . 'xsd' . \DIRECTORY_SEPARATOR . 'maindoc' . \DIRECTORY_SEPARATOR . 'UBL-Invoice-2.1.xsd';
+
+    public static function validateXSD(\DOMDocument $xml): bool|array
+    {
+        libxml_use_internal_errors(true);
+
+        if (!$xml->schemaValidate(self::XSD_PATH)) {
+            $errors = libxml_get_errors();
+            libxml_use_internal_errors(false);
+
+            return $errors;
+        }
+
+        return true;
+    }
 }
