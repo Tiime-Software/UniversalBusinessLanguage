95f1c4f54d505474dba8b2df5d74e5f5fa14437a
Samuel <133663914+samueld-pdp@users.noreply.github.com>
2024-11-18 11:39:02 +0100

    chore: change itemTypeCode reference (#18)

--- src/PeppolBIS/CreditNote/DataType/Aggregate/CommodityClassification.php
+++ src/PeppolBIS/CreditNote/DataType/Aggregate/CommodityClassification.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tiime\UniversalBusinessLanguage\PeppolBIS\CreditNote\DataType\Aggregate;
 
-use Tiime\EN16931\DataType\ItemTypeCode;
+use Tiime\EN16931\Codelist\ItemTypeCodeUNTDID7143 as ItemTypeCode;
 
 class CommodityClassification
 {
