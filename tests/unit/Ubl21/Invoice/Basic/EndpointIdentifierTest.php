8ba69531cac0e54ff7a8236604891d4f36b1e6eb
samueld-pdp <samuel.dechomets@tiimepdp.com>
2024-11-19 10:09:31 +0100

    chore: change BT-49 and BT-34 cardinality for UBL21

--- tests/unit/Ubl21/Invoice/Basic/EndpointIdentifierTest.php
+++ tests/unit/Ubl21/Invoice/Basic/EndpointIdentifierTest.php
@@ -3,8 +3,8 @@
 namespace Tiime\UniversalBusinessLanguage\Tests\unit\Ubl21\Invoice\Basic;
 
 use Tiime\EN16931\Codelist\ElectronicAddressSchemeCode as ElectronicAddressScheme;
-use Tiime\UniversalBusinessLanguage\Ubl21\Invoice\DataType\Basic\EndpointIdentifier;
 use Tiime\UniversalBusinessLanguage\Tests\helpers\BaseXMLNodeTestWithHelpers;
+use Tiime\UniversalBusinessLanguage\Ubl21\Invoice\DataType\Basic\EndpointIdentifier;
 
 class EndpointIdentifierTest extends BaseXMLNodeTestWithHelpers
 {
@@ -24,11 +24,6 @@ XML;
 <Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
   <cbc:EndpointID schemeID="EM">VendeurCanal1.00017@100000009.ppf</cbc:EndpointID>
   <cbc:EndpointID schemeID="EM">VendeurCanal1.00017@100000009.ppf</cbc:EndpointID>
-</Invoice>
-XML;
-
-    protected const XML_INVALID_NO_CODE = <<<XML
-<Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
 </Invoice>
 XML;
 
@@ -52,13 +47,6 @@ XML;
     {
         $this->expectException(\Exception::class);
         $currentElement = $this->loadXMLDocument(self::XML_INVALID_MULTIPLE_CODES);
-        EndpointIdentifier::fromXML($this->xpath, $currentElement);
-    }
-
-    public function testCannotBeCreatedFromNoCode(): void
-    {
-        $this->expectException(\Exception::class);
-        $currentElement = $this->loadXMLDocument(self::XML_INVALID_NO_CODE);
         EndpointIdentifier::fromXML($this->xpath, $currentElement);
     }
 
