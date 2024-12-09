911383db6c83deba955d2fff33bc16e3e40440b2
samueld-pdp <samuel.dechomets@tiimepdp.com>
2024-12-09 17:01:04 +0100

    fix: fix c/c charges setter issue

--- src/PeppolBIS/Invoice/UniversalBusinessLanguage.php
+++ src/PeppolBIS/Invoice/UniversalBusinessLanguage.php
@@ -675,7 +675,7 @@ class UniversalBusinessLanguage implements UniversalBusinessLanguageInterface
             }
         }
 
-        $this->allowances = $charges;
+        $this->charges = $charges;
 
         return $this;
     }
